7b84360d9b974c2d9055246d59992fb54f05d50d
cccr <1781587589@qq.com>
2019-09-11 22:39:41 +0800

    ➕ 在线修改用户密码

--- mmall/src/main/java/com/mmall/controller/portal/UserController.java
+++ mmall/src/main/java/com/mmall/controller/portal/UserController.java
@@ -75,7 +75,8 @@ public class UserController {
     public ServerResponse<String> forgetRestPassword(String username,String passwordNew,String forgetToken){
         return iUserService.forgetResetPassword(username,passwordNew,forgetToken);
     }
-
+    @RequestMapping(value = "reset_password.do",method = RequestMethod.GET)
+    @ResponseBody
     public ServerResponse<String> resetPassword(HttpSession session,String passwordOld,String passwordNew){
         User user =(User)session.getAttribute(Const.CURRENT_USER);
         if(user==null){
