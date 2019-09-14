e8b25ca5b252345781a3837461ec337dbd63ca58
cccr <1781587589@qq.com>
2019-09-14 23:09:03 +0800

    实现查询查询产品的List接口

--- Springboot/demo/src/main/java/com/mmall/demo/service/IProductService.java
+++ Springboot/demo/src/main/java/com/mmall/demo/service/IProductService.java
@@ -1,5 +1,6 @@
 package com.mmall.demo.service;
 
+import com.github.pagehelper.PageInfo;
 import com.mmall.demo.common.ServerResponse;
 import com.mmall.demo.pojo.Product;
 import com.mmall.demo.vo.ProductDetailVo;
@@ -8,4 +9,5 @@ public interface IProductService {
     ServerResponse saveOrUpdateProduct(Product product);
     ServerResponse<String> serSaleStatus(Integer productId,Integer status);
     ServerResponse<ProductDetailVo> manegeProductDetail(Integer productId);
+    ServerResponse<PageInfo> getProductList(int pageNum, int pageSize);
 }
